a1e31323e9dcd1ece76b140caa9c1b1b239e0e00
IsaacHog <111504782+IsaacHog@users.noreply.github.com>
2022-08-31 08:35:32 +0200

    Mer kundvagn

--- kundvagn/webstorage.js
+++ kundvagn/webstorage.js
@@ -1,10 +1,8 @@
-function handlaProdukt() {
-			
-	var produktNamn = document.getElementById('produktNamn').innerText;
-	var produktPris = document.getElementById('produktPris').innerText;
-	localStorage.setItem(produktNamn, produktPris);
+function handlaProdukt(id) {
+	var produktNamn = document.getElementById('produktNamn'+id).innerText;
+	var produktPris = document.getElementById('produktPris'+id).innerText;
+	localStorage.setItem(produktNamn, produktPris);    
     getKundkorg();
-	visaKundkorg();
 	
 }
 
@@ -36,13 +34,15 @@ function visaKundkorg() { // Denna funktionen printar endast ut listan med objek
 			list += "<tr><td><i>empty</i></td>\n<td><i>empty</i></td></tr>\n";
 		}
 
-		document.getElementById('list').innerHTML = list;
+		document.getElementById('list').innerHTML = list; 
+        // visaKundkorg och getKundkorg kan inte delas då dessa typer av kod (getElementById = x) ger error om ingen html kod med id x existerar.
 	} else {
 		alert('Cannot save shopping list as your browser does not support HTML 5');
 	}
 }
 
 function getKundkorg() { // Denna funktionen "hämtar" kundkorgen sedan sätter id "antal produkter" till antalet objekt i localStorage
+    // MÅSTE LIGGA LÄNGST NER I HTML
 	if (CheckBrowser()) {
 		var key = "";
 		var list = "<tr><th>Item</th><th>Value</th></tr>\n";
